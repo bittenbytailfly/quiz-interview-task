dfbce148ecf767f438cb598fd9a2ca76cb283555
Ed Carter <bittenbytailfly@server.fake>
2018-05-12 12:49:50 +0100

    Simple EF6 Data Acess Layer project added.

---
+++ rest-service/InterviewTask.DataAccess/QuizQuestionDbContext.cs
@@ -0,0 +1,35 @@
+﻿using InterviewTask.BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewTask.DataAccess
+{
+    public class QuizQuestionDbContext : DbContext
+    {
+        public DbSet<QuizQuestion> QuizQuestions { get; set; }
+
+        public QuizQuestionDbContext()
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<QuizQuestion>()
+                .HasKey(k => k.QuizQuestionId);
+            modelBuilder.Entity<QuizQuestion>()
+                .Property(p => p.Question)
+                .IsRequired()
+                .HasMaxLength(255);
+            modelBuilder.Entity<QuizQuestion>()
+                .Property(p => p.Answer)
+                .IsRequired()
+                .HasMaxLength(255);
+        }
+    }
+}
---
+++ rest-service/InterviewTask.DataAccess/Repositories/QuizQuestionRepository.cs
@@ -0,0 +1,33 @@
+﻿using InterviewTask.BusinessLogic.Interfaces;
+using InterviewTask.BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewTask.DataAccess.Repositories
+{
+    public class QuizQuestionRepository : IQuizQuestionRepository
+    {
+        public List<QuizQuestion> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddQuestion(QuizQuestion question)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
